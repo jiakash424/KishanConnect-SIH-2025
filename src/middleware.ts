8934b14effc0584974de5215ff4053d124ad36cc
Akash <jiakash427@gmail.com>
2025-09-19 04:45:18 +0000

    Error: Failed to call `useTranslations` because the context from `NextIn

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match all pathnames except for static files
-  matcher: ['/', '/(en|hi|bn|te|ta|mr|pa)/:path*']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
